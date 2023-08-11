942573bd63ff1e65d80939222f867ed3cb03ef1e
Abdera7mane <59313921+Abdera7mane@users.noreply.github.com>
2023-08-11 23:09:39 +0100

    Fix URL port parsing #2
    `String#substr()` takes a length as a second argument, not a position to
    the end of the sub-string.

--- URL.gd
+++ URL.gd
@@ -11,28 +11,38 @@ static func parse_url(url: String) -> Dictionary:
 	
 	var path_delimiter: int = url.find("/")
 	var port_delimiter: int
+
+	if path_delimiter == -1:
+		path_delimiter = url.length()
 	
 	path = url.substr(path_delimiter)
 	if path.empty():
 		path = "/"
 	
 	var has_port: bool
-	
+	var host_length: int
+
 	# IPv6
 	if url[0] == "[":
 		var end: int = url.find("]")
-		if end != -1:
+		if end == -1:
 			port_delimiter = url.find(":", end)
 			has_port = port_delimiter != -1 and port_delimiter < path_delimiter
-			host = url.substr(0, end + 1 if not has_port else port_delimiter)
+			host_length = end + 1 if not has_port else port_delimiter
 	# IPv4
 	else:
 		port_delimiter = url.find(":")
 		has_port = port_delimiter != -1 and port_delimiter < path_delimiter
-		host = url.substr(0, path_delimiter if not has_port else port_delimiter)
+		host_length = path_delimiter if not has_port else port_delimiter
 	
+	host = url.substr(0, host_length)
+
 	if has_port:
-		port = url.substr(port_delimiter, path_delimiter).to_int()
+		var port_length: int =  path_delimiter - port_delimiter \
+		                     if path_delimiter > 0 \
+	                       else url.length() - port_delimiter
+
+		port = url.substr(port_delimiter, port_length).to_int()
 	
 	return {
 		host = host,
@@ -46,3 +56,4 @@ static func must_reconnect(old: Dictionary, new: Dictionary) -> bool:
 			or not old.host.empty()\
 			and (old.host != new.host or old.use_ssl != new.use_ssl)\
 			or old.port != new.port
+
